53e73f87eb77fa5f73900f2ffb913f01da8e156a
Harshit Panwar <harshitpanwar234567@gmail.com>
2021-06-08 00:29:33 +0530

    Made recycler view adapter NotesRVAdapter.kt

--- app/src/main/java/com/example/notesmakingapp/MainActivity.kt
+++ app/src/main/java/com/example/notesmakingapp/MainActivity.kt
@@ -2,10 +2,53 @@ package com.example.notesmakingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), INotesRVAdapter {
+
+    lateinit var viewModel: NoteViewModel
+    lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this,
+            ViewModelProvider.AndroidViewModelFactory
+                .getInstance(application))
+                .get(NoteViewModel::class.java)
+
+
+        recyclerView = findViewById(R.id.recyclerview)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val adapter = NotesRVAdapter(this, this)
+
+        recyclerView.adapter = adapter
+
+
+
+
+        viewModel.allNotes.observe(this, Observer {list ->
+            list?.let {
+                adapter.updateList(it)
+            }
+        })
+
     }
+
+    override fun onItemClicked(note: Note) {
+        viewModel.deleteNote(note)
+    }
+
+    fun submitData(view: View) {
+
+
+
+    }
+
+
 }
