0bf0ab2873c665c78d377628f38f10722337395d
Harshit Panwar <harshitpanwar234567@gmail.com>
2021-06-08 00:49:28 +0530

    implemented recycler view and app functioning perfectly

--- app/src/main/java/com/example/notesmakingapp/MainActivity.kt
+++ app/src/main/java/com/example/notesmakingapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.notesmakingapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -10,27 +12,40 @@ import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity(), INotesRVAdapter {
 
-    lateinit var viewModel: NoteViewModel
-    lateinit var recyclerView: RecyclerView
+    private lateinit var viewModel: NoteViewModel
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var inputText: EditText
+    private lateinit var addButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+//all components declaration
+
+        recyclerView = findViewById(R.id.recyclerview)
+        inputText = findViewById(R.id.input)
+        addButton = findViewById(R.id.addButton)
+
+//creating view model instance in the main activity
         viewModel = ViewModelProvider(this,
             ViewModelProvider.AndroidViewModelFactory
                 .getInstance(application))
                 .get(NoteViewModel::class.java)
 
+//handling button click event
+        addButton.setOnClickListener{
+            submitData()
+        }
 
-        recyclerView = findViewById(R.id.recyclerview)
+
+//setting attributes of recycler view
         recyclerView.layoutManager = LinearLayoutManager(this)
         val adapter = NotesRVAdapter(this, this)
-
         recyclerView.adapter = adapter
 
-
-
+//observing data from the view model
 
         viewModel.allNotes.observe(this, Observer {list ->
             list?.let {
@@ -44,9 +59,13 @@ class MainActivity : AppCompatActivity(), INotesRVAdapter {
         viewModel.deleteNote(note)
     }
 
-    fun submitData(view: View) {
 
+    fun submitData() {
+        val noteText = inputText.text.toString()
+        if(noteText.isNotEmpty()){
+            viewModel.insertNote(Note(noteText))
 
+        }
 
     }
 
